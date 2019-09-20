208f076667952ddf4cb56e03817b07f3ba214e09
RobertGarcia1020 <garcia.robert1020@gmail.com>
2019-09-20 17:53:10 -0500

    Se añade UID
    Se implementa limpieza eliminando espacios salto de linea \n en title y de genera uid para el DtaFrame

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -1,6 +1,7 @@
 
 import argparse
 import logging
+import hashlib
 logging.basicConfig(level = logging.INFO)
 from urllib.parse import urlparse
 
@@ -16,6 +17,52 @@ def main(filename):
     newspaper_uid = _extract_newspapaer_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_newlines_from_body(df)
+    return df
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uid foreach row')    
+
+    uids = (df.apply(lambda row: hashlib.md5(bytes(row['url'].encode())) , axis=1)
+              .apply(lambda hash_object: hash_object.hexdigest())
+           )
+    
+    df['uid'] = uids
+    return df.set_index('uid')
+
+def _remove_newlines_from_body(df):
+    logger.info('removing new lines from body')
+
+    stripped_body = (df
+                        .astype(str)
+                        .apply(lambda row: row['body'], axis = 1)
+                        .apply(lambda body: list(body))
+                        .apply(lambda letters: 
+                                list(map(lambda letter: letter.replace('\n',''), letters)))
+                        .apply(lambda letters: ''.join(letters))
+        
+                    )
+    df['body'] = stripped_body
+    return df
+    
+
+#  Esta funciìn toma el df y remplaza los NAN o NUlL del title
+#  por un title que se encuentra en la url
+def _fill_missing_titles(df):
+    logger.info('Filling missing tittles')
+    missing_titles_mask = df['title'].isna()
+    #print(missing_titles_mask)
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .astype(str).applymap(lambda title: title.split('_'))
+                        .applymap(lambda title_word_list: ''.join(title_word_list))    
+                     )
+
+    #print(missing_titles.loc[:, 'missing_titles'])
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
     return df
 
 def _extract_host(df):
