b9e8ac67ae3da1b2110181489fe2f030661e3024
RobertGarcia1020 <garcia.robert1020@gmail.com>
2019-09-28 13:12:17 -0500

    Fin
    Termianos la limpieza de los datos

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -4,6 +4,8 @@ import logging
 import hashlib
 logging.basicConfig(level = logging.INFO)
 from urllib.parse import urlparse
+import nltk
+from nltk.corpus import stopwords
 
 import pandas as pd
 
@@ -20,7 +22,32 @@ def main(filename):
     df = _fill_missing_titles(df)
     df = _generate_uids_for_rows(df)
     df = _remove_newlines_from_body(df)
+    df = _tokenize_column(df, 'title')
+    df = _tokenize_column(df, 'body')
+    df = _remove_duplicate_entries(df,'title')
+    df = _drop_rows_with_missing_values(df)
+    df = _remove_filas_name(df, 'body')
+    _save_data(df,filename)
     return df
+
+def _remove_filas_name(df, column_name):
+    return df[df[column_name] != 'nan']
+
+def _remove_duplicate_entries(df, column_name):
+    logger.info('Removing duplicate entries')
+    df.drop_duplicates(subset=[column_name], keep = 'first', inplace=True)
+
+    return df
+
+def _drop_rows_with_missing_values(df):
+    logger.info('Dropping rows with missing values')
+    return df.dropna()
+
+def _save_data(df, filename):
+    cleanFilename = 'clean_{}'.format(filename)
+    logger.info('Saving data at location {}'.format(cleanFilename))
+    df.to_csv(cleanFilename)
+
 
 def _generate_uids_for_rows(df):
     logger.info('Generating uid foreach row')    
@@ -32,6 +59,20 @@ def _generate_uids_for_rows(df):
     df['uid'] = uids
     return df.set_index('uid')
 
+def _tokenize_column(df, column_name):
+    logger.info('Tokenizing token for column {}'.format(column_name))
+
+    stop_words = set(stopwords.words('spanish'))
+    colum_tokenizer = (df.dropna().astype(str)
+                                 .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                                 .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                                 .apply(lambda tokens: list(map(lambda token:token.lower(), tokens)))
+                                 .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                                 .apply(lambda valid_word_list: len(valid_word_list)))
+           
+    df['n_tokens_{}'.format(column_name)] = colum_tokenizer
+    return df
+
 def _remove_newlines_from_body(df):
     logger.info('removing new lines from body')
 
@@ -41,6 +82,8 @@ def _remove_newlines_from_body(df):
                         .apply(lambda body: list(body))
                         .apply(lambda letters: 
                                 list(map(lambda letter: letter.replace('\n',''), letters)))
+                        .apply(lambda letters: 
+                                list(map(lambda letter: letter.replace('\r',''), letters)))
                         .apply(lambda letters: ''.join(letters))
         
                     )
